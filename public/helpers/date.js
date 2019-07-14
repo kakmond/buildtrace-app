fbd5b3a788a629627e9160ba72e66d6f9b45fd73
kakmond <wongsathorn.pa@ku.th>
2019-07-15 01:41:53 +0900

    Initial check-in with index and graph page

---
+++ public/helpers/date.js
@@ -0,0 +1,7 @@
+module.exports = {
+    date: function (timestamp) {
+        var date = new Date(timestamp * 1000);
+        var formattedDate = ('0' + date.getDate()).slice(-2) + '/' + ('0' + (date.getMonth() + 1)).slice(-2) + '/' + date.getFullYear() + ' ' + ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2) + ':' + ('0' + date.getSeconds()).slice(-2);
+        return formattedDate
+    }
+}
---
+++ routes/index.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var router = express.Router();
+var fs = require('fs');
+var bzip = require('bzip-deflate')
+
+var web3 = require('web3');
+var endpoint = "http://127.0.0.1:8545" // ganache address
+web3 = new web3(new web3.providers.HttpProvider(endpoint));
+web3.eth.defaultAccount = web3.eth.accounts[0] // set account as sender
+
+/* TraceStorage smart contract configuration */
+var contract_Address = '0xCdCC578919e59A6F8eFFAb9E8a39389a36Ef763D'
+var contract_ABI = "abi/TraceStorage.json";
+var json = JSON.parse(fs.readFileSync(contract_ABI));
+var abi = json.abi
+var contract = new web3.eth.Contract(abi, contract_Address);
+
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  var pkgSearch = req.query.search
+  contract.methods.getTrace().call().then((result) => {
+    var objList = []
+    for (var i = 0; i < result.length; i++) {
+      var ascii = web3.utils.hexToAscii(result[i])
+      var decompressedJSON = bzip(ascii).toString()
+      var object = JSON.parse(decompressedJSON)
+      if (pkgSearch && object.package != pkgSearch) { // check if the query is passed and matching
+        continue; // skip to next iteration
+      }
+      object.id = i
+      objList.push(object)
+    }
+    res.render('index', { traces: objList, search: pkgSearch });
+  });
+});
+
+/* GET graph page. */
+router.get('/build/:id', function (req, res, next) {
+  contract.methods.getTrace().call().then((result) => {
+    var hex = result[req.params.id]
+    if (hex) { // if trace data exists
+      var ascii = web3.utils.hexToAscii(hex)
+      var decompressedJSON = bzip(ascii).toString()
+      res.render('graph', { title: 'Express', graph: decompressedJSON });
+    }
+    else
+      res.render('error', { message: 'buildtrace ID: ' + req.params.id + ' cannot be found.' });
+  });
+
+});
+
+module.exports = router;
